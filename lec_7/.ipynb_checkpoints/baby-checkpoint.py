01d199284f9f49e1f6a754779a8c41b4c9bf320c
Rahul Janghu <rjanghu01@gmail.com>
2020-06-23 12:28:40 +0530

    8th

---
+++ lec_7/.ipynb_checkpoints/baby-checkpoint.py
@@ -0,0 +1,16 @@
+def add(a, b):  ## these parameters are formal parameters
+    """This is for summation of a and b
+    a: give value of a
+    b: give value of b
+    """
+    
+    return f"The addition of {a} and {b} is {a+b}"
+    
+def mul(a, b):  ## these parameters are formal parameters
+    """This is for multiplication of a and b
+    a: give value of a
+    b: give value of b
+    """
+    
+    return f"The addition of {a} and {b} is {a*b}"
+    
